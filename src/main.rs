37fce3003fb2f6d4950f03c8e2c00d73f396ece0
Bastiaan Marinus van de Weerd <bvandeweerd@forte.io>
2024-05-09 10:37:42 -0400

    Stage 2.

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,35 @@ fn main() -> anyhow::Result<()> {
 		".dbinfo" => {
 			let mut file = File::open(&database_path)
 				.with_context(|| format!("opening database at {database_path:?}"))?;
-			let mut header = [0; 18];
-			file.read_exact(&mut header)?;
+			let file_size = file.metadata().context("accesing file metadata")?.len() as usize;
+			let mut header = [0; 100];
 
-			// The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-			let page_size = u16::from_be_bytes(header[16..18].try_into().unwrap());
+			file.read_exact(&mut header[..16]).context("reading first 16 bytes of header")?;
+			anyhow::ensure!(&header[..16] == b"SQLite format 3\0",
+				"expected header to start with string \"SQLite format 3\0\"");
+
+			file.read_exact(&mut header[16..]).context("reading remaining bytes of header")?;
+
+			let page_size = u16::from_be_bytes(header[16..18].try_into().unwrap()) as usize;
 			println!("database page size: {}", page_size);
+
+			let num_pages = u32::from_be_bytes(header[28..32].try_into().unwrap()) as usize;
+			anyhow::ensure!(file_size == num_pages * page_size,
+				"expected file size of {} but found {file_size}", num_pages * page_size);
+
+			let freelist_page = u32::from_be_bytes(header[32..36].try_into().unwrap());
+			anyhow::ensure!(freelist_page == 0, "expected no freelist page");
+
+			let mut page = vec![0u8; page_size];
+			page[..100].copy_from_slice(&header[..]);
+			file.read_exact(&mut page[100..])
+				.context("reading rest of first page")?;
+
+			anyhow::ensure!(page[100] == 0x0d,
+				"expected first page to be a leaf table b-tree page");
+			let num_cells = u16::from_be_bytes(page[103..105].try_into().unwrap());
+			// The `sqlite_schema` table contains one cell per table
+			println!("number of tables: {num_cells}");
 		}
 		_ => anyhow::bail!("unsupported command {command:?}"),
 	}
