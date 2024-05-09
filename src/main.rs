dd347a727472a750fd05ed390dc238b08b0660fa
Bastiaan Marinus van de Weerd <bvandeweerd@forte.io>
2024-05-09 09:49:19 -0400

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
-use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() -> Result<()> {
-	// Parse arguments
-	let args = std::env::args().collect::<Vec<_>>();
-	match args.len() {
-		0 | 1 => bail!("Missing <database path> and <command>"),
-		2 => bail!("Missing <command>"),
-		_ => {}
-	}
+use anyhow::Context as _;
 
-	// Parse command and act accordingly
-	let command = &args[2];
+
+fn main() -> anyhow::Result<()> {
+	// Parse arguments
+	let mut args = std::env::args();
+	let database_path = args.nth(1).context("expected <database path> argument")?;
+	let command = args.next().context("expected <command> argument")?;
+
 	match command.as_str() {
 		".dbinfo" => {
-			let mut file = File::open(&args[1])?;
-			let mut header = [0; 100];
+			let mut file = File::open(&database_path)
+				.with_context(|| format!("opening database at {database_path:?}"))?;
+			let mut header = [0; 18];
 			file.read_exact(&mut header)?;
 
 			// The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-			let page_size = u16::from_be_bytes([header[16], header[17]]);
-
+			let page_size = u16::from_be_bytes(header[16..18].try_into().unwrap());
 			println!("database page size: {}", page_size);
 		}
-		_ => bail!("Missing or invalid command passed: {}", command),
+		_ => anyhow::bail!("unsupported command {command:?}"),
 	}
 
 	Ok(())
